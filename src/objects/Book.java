34fc006c29a6b6510ff36144bd1785e2a94ed197
stepa <clocworchky@gmail.com>
2022-12-20 20:02:24 +0300

    )

--- src/objects/Book.java
+++ src/objects/Book.java
@@ -20,7 +20,37 @@ public class Book {
     public void setRelease(int release) {
         this.release = release;
     }
+    }
+class Author {
+    private String name;
+
+    private String surname;
+
+    private String patronymic;
+
+
+    public Author(String name, String surname, String patronymic) {
+
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public String getPatronymic() {
+        return this.patronymic;
+    }
+
 }
+
+
 
 
 
--- src/objects/nameBook.java
+++ src/objects/nameBook.java
@@ -15,4 +15,24 @@ public class nameBook {
     public String getNameBook2() {
         return this.nameBook2;
     }
+
+}
+class authors {
+    String author1;
+    String author2;
+
+    public authors(String author1, String author2) {
+
+        this.author1 = author1;
+        this.author2 = author2;
+
+    }
+
+    public String getNameAuhtor1() {
+        return this.author1;
+    }
+
+    public String getNameAuhtor2() {
+        return this.author2;
+    }
 }
