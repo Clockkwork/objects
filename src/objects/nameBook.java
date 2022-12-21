6d71e10486243f541c278a7786dc71ce0073adfc
stepa <clocworchky@gmail.com>
2022-12-21 23:54:38 +0300

    )

--- src/objects/nameBook.java
+++ src/objects/nameBook.java
@@ -1,13 +1,13 @@
 package objects;
-
 public class nameBook {
     String nameBook1;
     String nameBook2;
 
-    public  nameBook (String nameBook1,String nameBook2){
+    public nameBook(String nameBook1, String nameBook2) {
         this.nameBook1 = nameBook1;
         this.nameBook2 = nameBook2;
     }
+
     public String getNameBook1() {
         return this.nameBook1;
     }
@@ -16,23 +16,24 @@ public class nameBook {
         return this.nameBook2;
     }
 
+    public static class authors {
+        String author1;
+        String author2;
+
+        public authors(String author1, String author2) {
+
+            this.author1 = author1;
+            this.author2 = author2;
+
+        }
+
+        public String getNameAuhtor1() {
+            return this.author1;
+        }
+
+        public String getNameAuhtor2() {
+            return this.author2;
+        }
+    }
 }
-class authors {
-    String author1;
-    String author2;
 
-    public authors(String author1, String author2) {
-
-        this.author1 = author1;
-        this.author2 = author2;
-
-    }
-
-    public String getNameAuhtor1() {
-        return this.author1;
-    }
-
-    public String getNameAuhtor2() {
-        return this.author2;
-    }
-}
